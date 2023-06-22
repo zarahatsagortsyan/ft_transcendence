4b51d5d87958d3e051b02893d6c279ab09777304
InesHovh <ineshovh@gmail.com>
2023-06-22 22:13:42 +0400

    frontend

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -37,7 +37,7 @@ import Game from './pages/Game';
 
 function App() {
   // Dummy chat messages for testing
-  const chatMessages: string[] = [];
+  const chatMessages: { [channelName: string]: string[] } = {};
 
   return (
     <Container>
@@ -53,3 +53,77 @@ function App() {
 }
 
 export default App;
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { Container } from 'react-bootstrap';
+// import Login from './pages/Login';
+// import SetNick from './pages/SetNick';
+// import Profile from './pages/Profile';
+// import Chat from './pages/Chat';
+// import Game from './pages/Game';
+
+// function App() {
+//   // Dummy chat messages for testing
+//   const chatMessages: string[] = [];
+
+//   return (
+//     <Container>
+//       <Router>
+//         <Routes>
+//           <Route path="/" element={<Login />} />
+//           <Route path="/setnick" element={<SetNick />} />
+//           <Route path="/profile" element={<Profile />} />
+//           <Route path="/profile/chat" element={<Chat messages={chatMessages} />} />
+//           <Route path="/profile/game" element={<Game />} />
+//         </Routes>
+//       </Router>
+//     </Container>
+//   );
+// }
+
+// export default App;
+
+
+// import React, { useState, useEffect } from 'react';
+
+// interface User {
+//   id: number;
+//   name: string;
+//   email: string;
+// }
+
+// const App: React.FC = () => {
+//   const [users, setUsers] = useState<User[]>([]);
+
+//   useEffect(() => {
+//     fetchUsers();
+//   }, []);
+
+//   const fetchUsers = async () => {
+//     try {
+//       const response = await fetch('/api/users');
+//       const data = await response.json();
+//       setUsers(data);
+//     } catch (error) {
+//       console.error('Error fetching users:', error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Users</h1>
+//       <ul>
+//         {users.map(user => (
+//           <li key={user.id}>
+//             {user.name} - {user.email}
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default App;
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -16,3 +16,14 @@ root.render(
   </React.StrictMode>
 );
 
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
