4799e60b891aeff2ef4205f9b0a257705525d695
SimaAlaverdyan <sima.alaverdyan.02@gmail.com>
2023-07-30 15:49:20 +0400

    added ormconfig

---
+++ api/src/ormconfig.ts
@@ -0,0 +1,20 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+import { config } from "dotenv";
+
+config();
+
+const configg :TypeOrmModuleOptions=
+{
+    type : 'postgres',
+    host : process.env.POSTGRES_HOST,
+    port : parseInt(process.env.POSTGRES_HOST),
+    database : process.env.POSTGRES_DBATABASE as string,
+    username : process.env.POSTGRES_USE,
+    password : process.env.POSTGRES_PASSWORD as string,
+    autoLoadEntities: true,
+    synchronize: true,
+    // entities: ['src/**/*.entity.ts'],
+    ssl: false,
+}
+
+export default configg
