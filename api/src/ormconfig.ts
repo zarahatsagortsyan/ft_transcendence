655913dac76f4cc9ba402d550fed81d9792713e0
SimaAlaverdyan <sima.alaverdyan.02@gmail.com>
2023-08-01 11:45:30 +0400

    orm changes

--- api/src/ormconfig.ts
+++ api/src/ormconfig.ts
@@ -5,16 +5,17 @@ config();
 
 const configg :TypeOrmModuleOptions=
 {
+    name:'default',
     type : 'postgres',
     host : process.env.POSTGRES_HOST,
     port : parseInt(process.env.POSTGRES_PORT),
-    database : process.env.POSTGRES_DB as string,
+    database : process.env.POSTGRES_DATABASE as string,
     username : process.env.POSTGRES_USE,
     password : process.env.POSTGRES_PASSWORD as string,
     autoLoadEntities: true,
     synchronize: true,
     // entities: ['src/**/*.entity.ts'],
-    ssl: false,
+    // ssl: false,
 }
 
 export default configg
